6e8722650a92f7bd04ea3b7a3f41aff0ee4cb3e6
flug8 <80972789+flug8@users.noreply.github.com>
2023-07-05 18:22:00 +0200

    testing camera setup

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
-import * as THREE from 'three';
+//initialize scene
 
 const scene = new THREE.Scene();
+const aspectRatio = window.innerWidth / window.innerHeight;
+const sceneHeight = 200;
+const sceneWidth = sceneHeight * aspectRatio;
+const camera = new THREE.OrthographicCamera(-sceneWidth / 2, sceneWidth / 2, sceneHeight / 2, -sceneHeight / 2, -100, 100);
+camera.position.set(sceneWidth / 2,sceneHeight / 2);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// test
+
+const geometry = new THREE.BoxGeometry(10,10,10);
+const material = new THREE.MeshBasicMaterial({color: 0xff0000});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+//background plate
+
+const plate = new THREE.Mesh(new THREE.PlaneGeometry(window.innerWidth, window.innerHeight), new THREE.MeshBasicMaterial({color: 0x0000ff}));
+scene.add(plate);
+
+
+
+
+
+function setGameBackgroundColor(color){
+    plate.material.color.setHex(color);
+}
+
+
+
+
+// animate
+
+function animate(){
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    cube.rotation.z += 0.01;
+}
+animate();
